1d904cc45733f47b3f3a4193a05c4eb60acbaa6d
Jonas Gembala <jonas.gembala@stud.th-deg.de>
2025-04-22 15:19:22 +0200

    22.04 Enemy test sachen und Player wiederholung

---
+++ Assets/Scripts/Enemy/Enemy1.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Enemy1 : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Rigidbody2D rb;
+    private Vector3 direction;
+    [SerializeField] private float moveSpeed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //face the player
+        if(PlayerController.Instance.transform.position.x > transform.position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+        // move towards the player
+        moveSpeed = 1.0f;
+
+        direction = (PlayerController.Instance.transform.position - transform.position).normalized;
+
+        rb.linearVelocity= new Vector2 (direction.x * moveSpeed, direction.y * moveSpeed);
+
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController Instance;
 
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed;
     public Vector3 playerMoveDirection;
 
-    
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else{
+            Instance = this;
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
-        playerMoveDirection = new Vector2(inputX, inputY).normalized;
+        playerMoveDirection = new Vector3(inputX, inputY).normalized;
+
+        animator.SetFloat("moveX", inputX);
+        animator.SetFloat("moveY", inputY);
+
+        if(playerMoveDirection== Vector3.zero)
+        {
+            animator.SetBool("moving", false);
+        }
+        else
+        {
+            animator.SetBool("moving", true);
+        }
 
 
 
